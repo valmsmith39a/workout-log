002a0f88c7826dbff5382222c795a1f59e08f271
valmsmith39a <valmsmith39a@gmail.com>
2016-06-10 19:15:54 -0700

    creates add a widget button and wires it up

--- src/components/index.js
+++ src/components/index.js
@@ -2,16 +2,28 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.addAWidget = this.addAWidget.bind(this)
+  }
+
+  addAWidget() {
+    console.log('add a widget');
+  }
+
   renderWidgets() {
     return (
       <div>one widget</div>
     )
   }
-  
+
   render() {
     return(
       <div>
         <p>List of Widgets</p>
+        <button onClick={this.addAWidget}>Add a Widget</button>
+        <br />
         {this.renderWidgets()}
       </div>
     )
